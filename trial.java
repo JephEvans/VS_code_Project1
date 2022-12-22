a72e37e01b0f6bde1c9e74d28f8c3fc2def76da2
JephEvans <115298903+JephEvans@users.noreply.github.com>
2022-12-22 23:26:57 +0100

    Drag and drop files to test add files.

---
+++ trial.java
@@ -0,0 +1,5 @@
+class trial{
+    public static void main(String[] arg){
+        System.out.println("Is it working");
+    }
+}
---
+++ tryal.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main(){
+
+    printf("Subscribe Now!");
+    return 0;
+}
